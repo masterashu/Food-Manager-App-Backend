6d928a05bca838727ef0868f47f7fcb7b3adb3dd
Ketan Lambat <ketan.lambat@ymail.com>
2020-01-29 23:58:32 +0530

    routes modified

--- food_app/menu/routes.py
+++ food_app/menu/routes.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
-    path('opt_out/today', TodayMealOptOutView.as_view(), name='api_opt_out_today'),
+    path('menu/', MenuGetView.as_view(), name='api_get_menu'),
     path('opt_out/', MealOptOutView.as_view(), name='api_opt_out'),
-    # path('',)
+    path('opt_out/today/', TodayMealOptOutView.as_view(), name='api_opt_out_today'),
+    path('feedback/', FeedbackGivenView.as_view(), name='api_feedback_given'),
+    path('feedback/<int:month>/', MonthlyFeedbackGivenView.as_view(), name='api_monthly_feedback_given'),
 ]
