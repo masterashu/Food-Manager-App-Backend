65c051894a076d6077a3827010da62ac91ef938f
Ketan Lambat <ketan.lambat@ymail.com>
2020-01-31 14:17:09 +0530

    circular dependency removed

--- food_app/registration/models.py
+++ food_app/registration/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from menu.models import *
 from registration.validators import validate_phone
 
 
@@ -8,5 +7,3 @@ class User(AbstractUser):
     name = models.CharField(max_length=20, unique=True, null=False)
     email = models.EmailField(unique=True, max_length=254)
     contact_no = models.CharField(max_length=13, validators=[validate_phone])
-    meal_opted_out_from = models.ForeignKey(MealOptedOut, on_delete=models.CASCADE)
-    feedback_given = models.ForeignKey(Feedback, on_delete=models.CASCADE)
--- food_app/registration/routes.py
+++ food_app/registration/routes.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpattern = [
+urlpatterns = [
     path('createuser/', UserAccountCreateView.as_view(), name='api_create_user'),
     path('get_user_details/', UserDetailView.as_view(), name='api_user_details'),
 ]
