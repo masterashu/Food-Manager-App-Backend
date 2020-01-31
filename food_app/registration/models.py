9c6eb0abfc79637d89f8066d15182f6623b3813e
Ketan Lambat <ketan.lambat@ymail.com>
2020-01-31 14:23:18 +0530

    user model updated

--- food_app/registration/models.py
+++ food_app/registration/models.py
@@ -4,6 +4,4 @@ from registration.validators import validate_phone
 
 
 class User(AbstractUser):
-    name = models.CharField(max_length=20, unique=True, null=False)
-    email = models.EmailField(unique=True, max_length=254)
     contact_no = models.CharField(max_length=13, validators=[validate_phone], null=True, blank=True)
